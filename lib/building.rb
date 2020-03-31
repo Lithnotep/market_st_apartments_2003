4cf8f0c840750008a1ce14cbbf7390d7d8c5b40b
Max Mitrani <mjmitrani168@gmail.com>
2020-03-31 09:58:46 -0600

    Add add_unit method and test

--- lib/building.rb
+++ lib/building.rb
@@ -4,9 +4,14 @@ require 'pry'
 
 class Building
   attr_reader :units, :renters
+
   def initialize
     @units = []
     @renters = []
+  end
+
+  def add_unit(unit)
+    @units << unit
   end
 
 end
